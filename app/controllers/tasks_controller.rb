24b855aa1b17215334ec7f90eab3b1f26a9dd441
Jumpei Konishi <junpeko_0115@yahoo.co.jp>
2017-03-20 09:43:27 +0000

    修正３

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :correct_user, only: [:edit, :show]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  
   
   def new
     @task = current_user.tasks.new
@@ -24,7 +26,7 @@ class TasksController < ApplicationController
   end
   
   def update
-    if current_user.tasks.update(task_params)
+    if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
       redirect_to @task
     else
@@ -48,5 +50,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
   end
 end
